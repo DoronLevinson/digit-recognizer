0ef9bc9fda008ecf7fb268880f4c2606995a1324
Doron Levinson <doronlevinson@gmail.com>
2025-05-09 03:11:56 +0300

    decreasing knn size

--- inference.py
+++ inference.py
@@ -38,6 +38,8 @@ def load_knn_model(path="models/knn_model.joblib"):
     # for streamlit deployment
     X = pd.read_csv("models/X_knn.csv").values
     y = pd.read_csv("models/y_knn.csv").values.ravel()
+    X = X[:2000]
+    y = t[:2000]
 
     knn = KNeighborsClassifier(n_neighbors=10)
     knn.fit(X, y)
