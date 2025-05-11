ca39d68876fd57cb3e247c3ff708d019025dfa83
Doron Levinson <doronlevinson@gmail.com>
2025-05-11 16:21:32 +0300

    simple model added

--- inference.py
+++ inference.py
@@ -32,7 +32,8 @@ def load_dnn_model(path="models/simple_nn_mnist_model.pth"):
 def predict_dnn_digit(image: Image.Image, model):
     image = ImageOps.invert(image.convert("L")).resize((28, 28))
     img_array = np.array(image).astype(np.float32) / 255.0
-    img_tensor = torch.from_numpy(img_array).unsqueeze(0).unsqueeze(0)
+    img_tensor = torch.from_numpy(img_array).view(1, -1)
+    #img_tensor = torch.from_numpy(img_array).unsqueeze(0).unsqueeze(0)
 
     with torch.no_grad():
         output = model(img_tensor)
